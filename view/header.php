e17ef4dd1e1718815330e2e71c459b38d9ff1e71
fijey <fajarmukti9@gmail.com>
2020-01-30 16:04:30 +0700

    Add files via upload
    commit first

---
+++ view/header.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- My CSS  -->
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="dist/jquery.bxslider.css">
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="dist/jquery.bxslider.min.js"></script>
+
+  <title>Rumah Sakit ARS</title>
+</head>
+
+<body>
+   <!-- navbar -->
+
+   <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#"> <img src="images/logofix.png" width="80px" height="80px"></a>
+      <a class="navbar-brand" href="#">
+        Rumah Sakit ARS </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a href="landing_page.php" class="nav-item nav-link active">Home <span class="sr-only">(current)</span></a>
+          <a href="service.php"  class="nav-item nav-link" href="#">service</a>
+          <a href="about.php" class="nav-item nav-link " href="#">About</a>
+          <a href ="reservasi.php" class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Pasien Baru?</label></a>
+
+        </div>
+      </div>
+    </div>
+
+  </nav>
+
+  <!-- akhir navbar -->
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="jumbo1">
+      <div class="container">
+        <h1 class="display-4">Selamat datang di <br> Website Resmi Rumah Sakit ARS</h1>
+        <p class="lead">Kesehatan anda adalah prioritas kami </p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Akhir Jumbotron -->
+
+  <!-- Container -->
+  <div class="container">
+    <!-- Info Panel -->
+    <div class="row justify-content-center">
+      <div class="col -10 info-panel">
+        <div class="row">
+          <div class="col-lg">
+            <img src="images/employee.png" class="img100 float-left" alt="employee">
+            <h4>24 Hours</h4>
+            <P>Hubungi Call Center</P>
+          </div>
+          <div class="col-lg">
+            <img src="images/Schedule.png" class="img100 float-left" alt="Schedule">
+            <a href="table/tabel.html">
+              <h4>Cek Jadwal</h4>
+            </a>
+            <P>Cek Jam Praktek Dokter Spesialis Anda </P>
+          </div>
+          <div class="col-lg">
+            <img src="images/reservasi.png" class="img100 float-left" alt="reservasi">
+            <a href="reservasi.php">
+              <h4>Reservasi Online</h4>
+            </a>
+            <P>Reservasi tanpa Mengantri</P>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- akhir info -->
